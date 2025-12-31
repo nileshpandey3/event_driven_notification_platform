9768e1e70a357821be0ce398b45b833d3b3113e4
Nilesh Pandey <nileshpandey3@gmail.com>
2025-12-31 14:47:40 -0800

    cleanup and add more TODO's and run pylint

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 A unified, reliable mechanism for delivering user notifications from across different services with minimum latency
 
 
+### TODO: Explain the Project structure and flow 
+### TODO: Explain the tech stack used
+### TODO: Explain how to test
--- infrastructure/stacks/dynamodb_stack.py
+++ infrastructure/stacks/dynamodb_stack.py
@@ -1,6 +1,5 @@
 from aws_cdk import (
-    Stack,
-    aws_dynamodb as dynamodb,
+    Stack
 )
 from constructs import Construct
 
@@ -8,20 +7,6 @@ class DynamoDBStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # User Preferences table
-        self.user_preferences_table = dynamodb.Table(
-            self,
-            "UserPreferencesTable",
-            table_name="user_preferences",
-            partition_key=dynamodb.Attribute(
-                name="PK",
-                type=dynamodb.AttributeType.STRING
-            ),
-            sort_key=dynamodb.Attribute(
-                name="SK",
-                type=dynamodb.AttributeType.STRING
-            ),
-            billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST,
-            # removal_policy=dynamodb.RemovalPolicy.DESTROY  # Only for dev/test
-        )
+        #TODO: Write some code to create a Preferences table
+
 
