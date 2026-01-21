56122ded86ccc05808df4d27c5a7ea02a3738b0f
Nilesh Pandey <nileshpandey3@gmail.com>
2026-01-20 22:38:52 -0800

    TODO for the handler service

--- app/api/v1/preferences/service.py
+++ app/api/v1/preferences/service.py
@@ -5,35 +5,14 @@ Schema validation
 Idempotency
 """
 from sqlalchemy.orm import Session
-from app.models.user_preference import UserPreference
-from app.preferences.schemas import PreferenceCreate
+from app.api.v1.preferences.schemas import PreferencesCreate
 
 def create_or_update_preference(
     db: Session,
     user_id: str,
-    payload: PreferenceCreate,
+    payload: PreferencesCreate,
 ):
-    pref = (
-        db.query(UserPreference)
-        .filter(
-            UserPreference.user_id == user_id,
-            UserPreference.preference_type == payload.preference_type,
-        )
-        .first()
-    )
-
-    if pref:
-        pref.mandatory = payload.mandatory
-        pref.default_channel = payload.default_channel
-    else:
-        pref = UserPreference(
-            user_id=user_id,
-            preference_type=payload.preference_type,
-            mandatory=payload.mandatory,
-            default_channel=payload.default_channel,
-        )
-        db.add(pref)
-
-    db.commit()
-    db.refresh(pref)
-    return pref
+    """
+    TODO: Add logic for the handler service
+    which will validate/manipulate the incoming payload data and upsert
+    """
