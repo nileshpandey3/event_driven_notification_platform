90461f3414f8a74b9efd21cd79c16313c6ea9153
Rafael Brant Perrotti <rafaelbrantbh@gmai.com>
2026-01-28 22:50:06 -0300

    Implement add_user_preference

--- app/api/v1/preferences/routes.py
+++ app/api/v1/preferences/routes.py
@@ -1,13 +1,13 @@
 """
-This is the API layer, connects HTTP requests → service/repository → DynamoDB
+This is the API layer, connects HTTP requests → service/repository → DB
 """
 
 from fastapi import APIRouter, Depends, status
 
-from app.api.v1.preferences.schemas import PreferencesResponse
+from app.api.v1.preferences.schemas import PreferencesCreate, PreferencesResponse
 from app.api.v1.preferences.service import add_user_preference
-
 from app.core.auth import get_current_user
+from db.session import get_db
 
 router = APIRouter(prefix="/preferences", tags=["preferences"])
 
@@ -17,11 +17,15 @@ router = APIRouter(prefix="/preferences", tags=["preferences"])
     response_model=PreferencesResponse,
     status_code=status.HTTP_201_CREATED,
 )
-def create_preferences():
+def create_preferences(
+    body: PreferencesCreate,
+    user=Depends(get_current_user),
+    db=Depends(get_db),
+):
     """
     Create new preferences record for a user
     """
-    return add_user_preference()
+    return add_user_preference(body, user, db)
 
 
 @router.put("/")
